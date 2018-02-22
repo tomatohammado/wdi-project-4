85ff6f783cfecb30d4ce061d5ea3512aecd560d0
Hammad Malik <hmd.node@gmail.com>
2018-02-22 18:55:45 -0500

    Create TotalDoughnutChart component
    There are no colors, but hey, it works!

---
+++ src/components/TotalsDoughnutChart.vue
@@ -0,0 +1,40 @@
+<template>
+  <doughnut-chart :data="datacollection"></doughnut-chart>
+</template>
+
+<script>
+import DoughnutChart from './DoughnutChart.js'
+
+export default {
+  components: {
+    DoughnutChart
+  },
+  data () {
+    return {
+      datacollection: {
+        datasets: [{
+          data: [
+            651972,
+            167019,
+            195113,
+            419512,
+            102140,
+            547584,
+            170583,
+          ]
+        }],
+        labels: [
+          'Class: 181:06:12',
+          'Homework: 46:23:39',
+          'Labs: 54:11:53',
+          'Projects: 116:31:52',
+          'Outcomes: 28:22:20',
+          'MVP: 152:06:24',
+          'Bonus: 47:23:03'
+        ]
+      }
+    }
+  }
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img src="../assets/logo.png">
     <wdi-day></wdi-day>
+    <totals-doughnut-cart></totals-doughnut-cart>
   </div>
 </template>
 
 <script>
 import WdiDay from '@/components/WdiDay.vue'
+import TotalsDoughnutCart from '@/components/TotalsDoughnutChart.vue'
 
 export default {
   name: 'home',
   components: {
-    WdiDay
+    WdiDay,
+    TotalsDoughnutCart
   }
 }
 </script>
